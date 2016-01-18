6004035f6911e34c29e3f5cf212815425cfa4f2e
Aidan Coxon <aidan.coxon@yahoo.co.uk>
2016-01-18 21:38:02 +0000

    getting-home-v0.5
    memes
    
    also this is the new and updated fixed project

---
+++ Getting Home/Getting Home/Assets/4. Scripts/Conversation Scripts/NPCConversationScript.cs
@@ -0,0 +1,135 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using System.Collections;
+using System.Collections.Generic;
+using System;
+
+public class NPCConversationScript : MonoBehaviour 
+{
+	enum ConversationState
+	{
+		Opening,
+		Waiting,
+		OnComplete,
+		AlreadyComplete
+	};
+
+	ConversationState currentConvoState;
+
+	public ConversationScript openingConvo;
+	public ConversationScript waitingForCompleteConvo;
+	public ConversationScript onCompleteConvo;
+	public ConversationScript alreadyCompletedConvo;
+
+	bool playerIn;
+	bool currentlyInDialogue;
+	int currentConversationIndex;
+
+	List<ConversationEntry> CurrentDialogueList
+	{
+		get
+		{
+			switch (currentConvoState) 
+			{
+			case ConversationState.Opening:
+				return openingConvo.conversationEntries;
+				break;
+			}
+
+			return null;
+		}
+	}
+
+	public PlayerScript player;
+
+	DialogueUIScript dialogueUI;
+
+	public void Start()
+	{
+		GameObject uiObj = GameObject.FindGameObjectWithTag ("UI");
+
+		if (uiObj != null)
+		{
+			dialogueUI = uiObj.GetComponent<DialogueUIScript> ();
+		}
+
+		player = player.GetComponent<PlayerScript>();
+
+		player.enabled = true;	
+	}
+
+	public void Update()
+	{
+		if (Input.GetButtonDown("Fire1") && currentlyInDialogue == false && playerIn == true)
+		{
+			currentlyInDialogue = true;
+			currentConversationIndex = 0;
+
+			ConversationEntry convoEntry = CurrentDialogueList[currentConversationIndex];
+
+			dialogueUI.NewNode(convoEntry.npcName, convoEntry.dialogue, convoEntry.npcIcon, convoEntry.playerIcon, convoEntry.rightFaded, NextButtonPressed);
+		}
+	}
+
+	void NextButtonPressed()
+	{
+		currentConversationIndex ++;
+
+		if (currentConversationIndex >= CurrentDialogueList.Count) 
+		{
+			//End the conversation
+		} 
+		else 
+		{
+			ConversationEntry convoEntry = CurrentDialogueList[currentConversationIndex];
+			
+			dialogueUI.NewNode(convoEntry.npcName, convoEntry.dialogue, convoEntry.npcIcon, convoEntry.playerIcon, convoEntry.rightFaded, NextButtonPressed);
+		}
+	}
+
+	void OnTriggerEnter(Collider other)
+	{
+		if (other.tag == "Player") 
+		{
+			playerIn = true;
+		}
+	}
+
+	void OnTriggerExit(Collider other)
+	{
+		if (other.tag == "Player") 
+		{
+			playerIn = false;
+		}
+	}
+
+	//This disables the player script. Player will not be able to move whilst this is active. Don't active it at the same time as EnablePlayerScript as this'll just be overrided.
+	//Also ensure that this isn't executed in the player script!
+	public void DisablePlayerScript()
+	{
+		if (player.isActiveAndEnabled == true) 
+		{
+			player.enabled = false;	
+			Debug.Log ("Is player active? " + player.enabled);
+		}		
+	}
+
+	//This enables the player script. Player will be able to control the character again when active. Don't active it at the same time as DisablePlayerScript as this'll just be overrided.
+	//Also ensure that this isn't executed in the player script!
+	public void EnablePlayerScript()
+	{
+		if (player.isActiveAndEnabled == false) 
+		{
+			player.enabled = true;	
+			Debug.Log ("Is player active? " + player.enabled);
+		}
+	}
+}
+
+
+
+
+
+
+
---
+++ Getting Home/Getting Home/Assets/4. Scripts/Interaction Scripts/MouseHighlight.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MouseHighlight : MonoBehaviour 
+{
+	//This script manages the mouse highlighting, currently unfinished.
+
+	private float blueMultiply = 3.50f;				
+	private float redGreenMultiply = 0.50f;			
+	
+	private Color originalColor;					//A reference to the object's original colour.
+	private Renderer rend;							//A reference to the object's renderer.
+	
+	private void Start()
+	{
+		rend = gameObject.GetComponent<Renderer>();
+		originalColor = rend.material.color;
+	}
+	
+	void OnMouseEnter()
+	{
+		AddHighlight();
+	}
+	
+	void OnMouseExit()
+	{
+		RemoveHighlight();
+	}
+	
+	private void  AddHighlight()
+	{
+		float red = originalColor.r * redGreenMultiply;
+		float blue = originalColor.b * blueMultiply;
+		float green = originalColor.g * redGreenMultiply;
+		
+		rend.material.color = new Color(red, green, blue);
+	}
+	
+	private void RemoveHighlight()
+	{
+		rend.material.color = originalColor;
+	}
+}
