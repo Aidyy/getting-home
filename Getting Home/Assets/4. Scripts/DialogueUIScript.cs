c4e2618b9334a2cdfe5223efcd931b4e7e92ee55
Aidan Coxon <aidan.coxon@yahoo.co.uk>
2016-01-18 19:26:00 +0000

    v0.4 update
    adds in:
    - object interaction (pickup, detection)
    - updated art assets
    - updated levels
    - updated developer test scenes
    - updated movement scripts
    - updated effect assets/scripts
    - updated standard UI assets
    - added main menu

---
+++ Getting Home/Assets/4. Scripts/DialogueUIScript.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using UnityEngine.Events;
+using System;
+
+public class DialogueUIScript : MonoBehaviour 
+{
+	public Image npcImage;
+	public Image playerImage;
+	public Text titleText;
+	public Text bodyText;
+	bool rightFaded;
+
+	Action NextPressed;
+
+	void Start()
+	{
+		if (npcImage == null) {
+			npcImage = transform.FindChild("DialoguePanel/ModalDialoguePanel/NPCImage").GetComponent<Image>();
+		}
+
+		if (playerImage == null) {
+			playerImage = transform.FindChild("DialoguePanel/ModalDialoguePanel/PlayerImage").GetComponent<Image>();
+		}
+
+		if (titleText == null) {
+			titleText = transform.FindChild("DialoguePanel/ModalDialoguePanel/TitleText").GetComponent<Text>();
+		}
+
+		if (bodyText == null) {
+			bodyText = transform.FindChild("DialoguePanel/ModalDialoguePanel/BodyText").GetComponent<Text>();
+		}
+	}
+
+	public void NewNode(string newTitle, string newBody, Sprite newNPCImage, Sprite newPlayerImage, bool isRightFaded, Action nextPressed)
+	{
+		titleText.text = newTitle;
+		bodyText.text = newBody;
+		npcImage.sprite = newNPCImage;
+		playerImage.sprite = newPlayerImage;
+
+		if (nextPressed != null) 
+		{
+			NextPressed += nextPressed;
+		}
+	}
+
+	void NextButtonPressed()
+	{
+		if (NextPressed != null) 
+		{
+			NextPressed();
+		}
+	}
+}
