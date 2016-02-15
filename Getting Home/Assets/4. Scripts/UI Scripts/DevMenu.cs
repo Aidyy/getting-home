afb01f379849aa62a26a6e612c43fd47e3ec3ece
Aidan Coxon <aidan.coxon@MININT-4HU1PLA.FWGC.COM>
2016-02-15 21:34:59 +0000

    getting-home-0.6
    - cleaned up git
    - fixed player movement
    - fixed collisions
    - updated seethru script
    - updated player character
    + added fog animations
    + added lighting textures

---
+++ Getting Home/Assets/4. Scripts/UI Scripts/DevMenu.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DevMenu : MonoBehaviour 
+{
+	public PlayerScript player;
+
+	float speedUp = 5;
+	float speedDown = 1;
+	float playerNormSpeed = 2;
+
+	// Use this for initialization
+	void Start () 
+	{
+		player = gameObject.GetComponent<PlayerScript>(); 
+	}
+
+	public void NormalPlayerSpeed()
+	{
+		player.speed = playerNormSpeed;
+	}
+
+	public void SuperSpeed()
+	{
+ 		player.speed += speedUp * Time.deltaTime;
+	}
+
+	public void SuperSlow()
+	{
+		player.speed -= speedDown * Time.deltaTime;
+	}
+
+//	public void ForceNextLevel()
+//	{
+//
+//	}
+//
+//	public void ForcePreviousLevel()
+//	{
+//
+//	}
+
+	public void ForceExit()
+	{
+		Application.Quit();
+	}
+}
