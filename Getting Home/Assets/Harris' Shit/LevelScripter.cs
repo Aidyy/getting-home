81917f0909112b50195c3abdbb58ce095c4a1770
Aidan Coxon <aidan.coxon@fwgc.com>
2016-03-31 20:48:28 +0100

    UI update
    the UI will now:
    -fade
    -display in order
    -bring up an exit panel (which does work)
    
    we need some UI assets to make them not so ugly, and figure out sizes
    and text content whatnot

---
+++ Getting Home/Assets/Harris' Shit/LevelScripter.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelScripter : MonoBehaviour {
+
+//	public GameObject Beaver;
+//	public GameObject Fox;
+//	public GameObject MotherBear;
+//	public GameObject BearCub;
+	public GameObject barrenTreeStump;
+	public GameObject BarrenTree;
+	public GameObject barrenTreeFallen;
+	public GameObject barrenTreeStump2;
+
+	public bool beaverObjCompleted;
+	public bool foxObjCompleted;
+	public bool motherBearObjCompleted;
+	public bool bearCubObjCompleted;
+
+	EventSpriteEnabler barren;
+	EventSpriteEnabler stump;
+	EventSpriteEnabler barrenFallen;
+	EventSpriteEnabler barrenStump;
+
+	// Use this for initialization
+	void Start () {
+		barren = BarrenTree.GetComponent<EventSpriteEnabler> ();
+		stump = barrenTreeStump.GetComponent<EventSpriteEnabler>();
+		barrenFallen = barrenTreeFallen.GetComponent<EventSpriteEnabler> ();
+		barrenStump = barrenTreeStump2.GetComponent<EventSpriteEnabler> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (beaverObjCompleted) {
+
+			if (BarrenTree != null)
+			{
+			barren.DestroyObject ();
+			}
+			if (barrenTreeStump != null)
+			stump.DestroyObject();
+			barrenFallen.SpriteEnable();
+			barrenStump.SpriteEnable();
+
+
+		}
+	}
+}
