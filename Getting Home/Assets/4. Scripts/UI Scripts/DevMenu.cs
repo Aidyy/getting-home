836034fa92054546966303a84cda7cd901437453
Freddie Bell <futureworks@FWGC.COM>
2016-04-28 21:45:19 +0100

    re-added assets
    updated level

---
+++ Getting Home/Assets/4. Scripts/UI Scripts/DevMenu.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DevMenu : MonoBehaviour 
+{
+
+	public GameObject Trees;
+	
+	void Update()
+	{
+		if(Input.GetKeyDown(KeyCode.O))
+		{
+			Trees.SetActive(false);
+		}
+		if(Input.GetKeyDown (KeyCode.P))
+		{
+			Trees.SetActive(true);
+		}
+	}
+}
