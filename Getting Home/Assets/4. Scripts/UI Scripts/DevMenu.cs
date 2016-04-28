68645849274f1676577767663954d43600c1e237
Aidan Coxon <aidan.coxon@fwgc.com>
2016-04-28 20:09:31 +0100

    getting-home-v0.05
    Fixed UI (Again)
    Implemented Dev Menu (see in-game for functions)
    Updated tags
    Animation ready

--- Getting Home/Assets/4. Scripts/UI Scripts/DevMenu.cs
+++ Getting Home/Assets/4. Scripts/UI Scripts/DevMenu.cs
@@ -3,45 +3,18 @@ using System.Collections;
 
 public class DevMenu : MonoBehaviour 
 {
-	public PlayerScript player;
 
-	float speedUp = 5;
-	float speedDown = 1;
-	float playerNormSpeed = 2;
-
-	// Use this for initialization
-	void Start () 
+	public GameObject Trees;
+	
+	void Update()
 	{
-		player = gameObject.GetComponent<PlayerScript>(); 
-	}
-
-	public void NormalPlayerSpeed()
-	{
-		player.speed = playerNormSpeed;
-	}
-
-	public void SuperSpeed()
-	{
- 		player.speed += speedUp * Time.deltaTime;
-	}
-
-	public void SuperSlow()
-	{
-		player.speed -= speedDown * Time.deltaTime;
-	}
-
-//	public void ForceNextLevel()
-//	{
-//
-//	}
-//
-//	public void ForcePreviousLevel()
-//	{
-//
-//	}
-
-	public void ForceExit()
-	{
-		Application.Quit();
+		if(Input.GetKeyDown(KeyCode.O))
+		{
+			Trees.SetActive(false);
+		}
+		if(Input.GetKeyDown (KeyCode.P))
+		{
+			Trees.SetActive(true);
+		}
 	}
 }
