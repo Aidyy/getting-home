15d3dbfd66c5768676682ca0b75573fce1869f7e
Aidan Coxon <aidan.coxon@fwgc.com>
2016-05-16 18:46:53 +0100

    getting-home-v0.567
    this is the branch that gets pushed

---
+++ Getting Home 0.576/Getting Home 0.575/Assets/4. Scripts/Conversation Scripts/ConversationScript.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System;
+
+[System.Serializable]
+public class ConversationEntry
+{
+	public string npcName;		//The name of the NPC talking
+	public string dialogue;		//The text that the NPC actually says
+	public Sprite playerIcon;	//The icon of the player in the conversation panel
+	public Sprite npcIcon;		//The icon of the NPC in the conversation panel
+	public bool rightFaded;		//Checks whether or not the conversation has a fade-out-in ending
+}
+
+public class ConversationScript : MonoBehaviour 
+{
+	public List <ConversationEntry> conversationEntries = new List<ConversationEntry>();
+	
+	public Action conversationFadedOut;
+	public Action conversationFininshed;
+	
+	public bool fadeOutConversation;
+}
