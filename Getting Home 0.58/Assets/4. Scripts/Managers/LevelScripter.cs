7af3f60c34b9374c46d974dcc0219d7e5fc10697
Aidan Coxon <aidan.coxon@fwgc.com>
2016-05-23 21:52:43 +0100

    getting-home-v0.58
    - updated path tiles
    - updated the island tile
    - began implementation of new fading script
    - fixed some layer sorting issues
    - began fixing the UI dialogue issues

---
+++ Getting Home 0.58/Assets/4. Scripts/Managers/LevelScripter.cs
@@ -0,0 +1,107 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelScripter : MonoBehaviour {
+
+//	public GameObject Beaver;
+//	public GameObject Fox;
+//	public GameObject MotherBear;
+//	public GameObject BearCub;
+	public GameObject barrenTreeStump;
+	public GameObject BarrenTree;
+	public GameObject barrenTreeFallen;
+	public GameObject barrenTreeStump2;
+
+	public GameObject beaver;
+	public GameObject motherBear;
+	public GameObject fox;
+	public GameObject bearCub;
+
+	public EventFadeScript eventFadingScript;
+
+	public NpcScript beaverScript;
+	public NpcScript motherBearScript;
+	public NpcScript foxScript;
+	public NpcScript bearCubScript;
+	NewChatScript bearChatScript;
+	NewChatScript foxChatScrupt;
+
+	public bool beaverObjCompleted;
+	public bool foxObjCompleted;
+	public bool motherBearObjCompleted;
+	public bool bearCubObjCompleted;
+
+	EventSpriteEnabler barren;
+	EventSpriteEnabler stump;
+	EventSpriteEnabler barrenFallen;
+	EventSpriteEnabler barrenStump;
+
+
+	// Use this for initialization
+	void Start () {
+		ScriptAttacher ();
+		barren = BarrenTree.GetComponent<EventSpriteEnabler> ();
+		stump = barrenTreeStump.GetComponent<EventSpriteEnabler>();
+		barrenFallen = barrenTreeFallen.GetComponent<EventSpriteEnabler> ();
+		barrenStump = barrenTreeStump2.GetComponent<EventSpriteEnabler> ();
+
+		eventFadingScript = eventFadingScript.GetComponent <EventFadeScript>();
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		FadeManager();
+
+//		if (foxScript.altObjectiveMet2) {
+//			Application.LoadLevel("Menu");
+//		}
+		bearCubObjCompleted = bearCubScript.objectiveMet;
+		if (foxChatScrupt.altObjectiveMet2)
+		{
+			eventFadingScript.FadeToBlack();
+			
+			Application.LoadLevel("Menu");
+		}
+		if (beaverObjCompleted) 
+		{
+			eventFadingScript.FadeToBlack();
+
+			if (BarrenTree != null)
+			{
+			barren.DestroyObject();
+			}
+			if (barrenTreeStump != null)
+			stump.DestroyObject();
+
+			if(barrenFallen != null)
+			barrenFallen.SpriteEnable();
+
+			if (barrenStump != null)
+			barrenStump.SpriteEnable();
+
+			eventFadingScript.FadeToClear();
+		}
+		if (bearCubObjCompleted) 
+		{
+			motherBearScript.objectiveMet = true;
+			bearChatScript.objectiveCompleted = true;
+		}
+	}
+
+	void FadeManager()
+	{
+		eventFadingScript.FadeToClear();
+	}
+
+	void ScriptAttacher()
+	{
+		beaverScript = beaver.GetComponent<NpcScript> ();
+		motherBearScript = motherBear.GetComponent<NpcScript> ();
+		foxScript = fox.GetComponent<NpcScript> ();
+		foxChatScrupt = fox.GetComponent<NewChatScript> ();
+		bearCubScript = bearCub.GetComponent<NpcScript> ();
+		bearChatScript = motherBear.GetComponent<NewChatScript> ();
+
+	}
+}
---
+++ Getting Home 0.58/Assets/4. Scripts/UI Scripts/EventFadeScript.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class EventFadeScript : MonoBehaviour
+{
+	public Image fadeImg;
+	public float fadeSpeed = 0f;	
+
+	public void FadeToClear()
+	{
+		// Lerp the colour of the image between itself and transparent.
+		fadeImg.color = Color.Lerp(fadeImg.color, Color.clear, fadeSpeed * Time.deltaTime);
+	}
+	
+	public void FadeToBlack()
+	{
+		// Lerp the colour of the image between itself and black.
+		fadeImg.color = Color.Lerp(fadeImg.color, Color.black, fadeSpeed * Time.deltaTime);
+	}
+}
