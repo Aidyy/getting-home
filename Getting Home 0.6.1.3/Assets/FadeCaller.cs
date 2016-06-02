32a8a71fd7abc1288a8207d9b65e33bd61ba724b
Aidan Coxon <aidan.coxon@fwgc.com>
2016-06-02 20:55:38 +0100

    getting-home-v0.6.1.3
    added fading script

---
+++ Getting Home 0.6.1.3/Assets/FadeCaller.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FadeCaller : MonoBehaviour 
+{
+	EventFadeScript fadeManager;
+	
+	void Start () 
+	{
+		fadeManager = GetComponent<EventFadeScript>();
+	}
+
+	void Update () 
+	{
+		fadeManager.StartCoroutine("DecreaseAlphaCoroutine");
+	}
+}
