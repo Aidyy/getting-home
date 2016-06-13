909940919981d30252ccac156d9a49dbf7628eef
Aidan Coxon <aidan.coxon@yahoo.co.uk>
2016-06-13 15:22:10 +0100

    getting-home-v0.62
    if this doesn't work i swear to fucking god i'm going to flip my shit
    and kms

---
+++ Getting Home 0.6.2/Assets/4. Scripts/ItemPickedUp.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ItemPickedUp : MonoBehaviour {
+
+	public Sprite Item_Axe;
+	public Sprite Item_Key;
+	public Sprite Item_PerfectLog;
+	public Sprite Item_BadLog;
+	string tempItem;
+
+	SpriteRenderer spriteRenderer;
+
+	// Use this for initialization
+	void Start () {
+		spriteRenderer = GetComponent<SpriteRenderer> ();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		PlayerScript parentScript = GetComponentInParent<PlayerScript> ();
+		if (parentScript.currentHeldItem == "nothingHeld") {
+			spriteRenderer.sprite = null;
+		} else if (parentScript.currentHeldItem == "Item_BadLog") {
+			spriteRenderer.sprite = Item_BadLog;
+		} else if (parentScript.currentHeldItem == "Item_Key") {
+			spriteRenderer.sprite = Item_Key;
+		} else if (parentScript.currentHeldItem == "Item_PerfectLog") {
+			spriteRenderer.sprite = Item_PerfectLog;
+		} else if (parentScript.currentHeldItem == "Item_Axe") {
+			spriteRenderer.sprite = Item_Axe;
+		} else if (parentScript.currentHeldItem == null) {
+			spriteRenderer.sprite = null;
+		}
+
+		
+	}
+}
